e5d287ffb7ccc00ca99f469ad9816ec8bf6f3975
SanjaySrc5489 <133635183+SanjaySrc5489@users.noreply.github.com>
2025-02-28 01:03:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,15 @@ pip install -r requirements.txt
 
 # Install Chromium for Playwright
 playwright install chromium
+
+# Install dependencies required for Chromium to run on Render
+apt update && apt install -y \
+    libnss3 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libdrm2 \
+    libxkbcommon0 \
+    libgbm1 \
+    libgtk-3-0 \
+    libasound2
